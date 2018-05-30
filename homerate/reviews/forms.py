c58d57bb5a1cb98df398ec57097fc7f454c6632b
edward <es916@ic.ac.uk>
2018-05-30 12:31:07 +0100

    Add very basic House database lookup and return house information.

---
+++ homerate/reviews/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import House
+
+class HouseForm(forms.ModelForm):
+
+    class Meta:
+        model = House
+        fields = ('Address',)
--- homerate/reviews/views.py
+++ homerate/reviews/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import House
+from .models import HouseReport
 # Create your views here.
+
+def house(request, number):
+    print(number)
+    house = House.objects.filter(house_id=number)
+    print(house)
+    if len(house) == 0:
+        return render(request, 'reviews/nohouse.html', {})
+
+    print(house[0])
+    return render(request, 'reviews/house.html', {'house' : house[0]})
