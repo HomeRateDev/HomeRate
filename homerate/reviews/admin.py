2e5d2b326a7d23e94b2496f5816dc13d007161d4
Zeshan Amjad <zeshan@zesh.me>
2018-05-30 11:07:59 +0100

    Register reviews models

--- homerate/reviews/admin.py
+++ homerate/reviews/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
++from .models import House, HouseReport
 
 # Register your models here.
+admin.site.register(House)
+admin.site.register(HouseReport)
--- homerate/reviews/models.py
+++ homerate/reviews/models.py
@@ -1,3 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class House(models.Model):
+	addr = models.CharField(max_length=200)
+	house_id = models.IntegerField()
+	date_created = models.DateTimeField(default=timezone.now)
+	reports = models.ManyToManyField('reviews.HouseReport', blank=True)
+
+	def __str__(self):
+		return self.addr
+
+
+class HouseReport(models.Model):
+    house_filed = models.ForeignKey('reviews.House', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    moved_in_date = models.DateField(blank=True, null=True)
+    description = models.TextField()
+    pros = models.TextField()
+    cons = models.TextField()
+    monthly_cost = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return str(self.house_filed) + " - " + str(self.author) + " - " + str(self.moved_in_date)
