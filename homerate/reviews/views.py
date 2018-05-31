09a4c66957c937cae9331f7bb250c5fd88602c78
edward <es916@ic.ac.uk>
2018-05-31 18:15:30 +0100

    Implement new review page and store reviews in database.

--- homerate/reviews/views.py
+++ homerate/reviews/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
 
-from .forms import HouseForm
+from .forms import HouseForm, HouseReportForm
 from .models import House
 from .models import HouseReport
 # Create your views here.
 
 
-def house(request, number):
+def house(request, id):
     # Query database for house with correct id
-    houses = House.objects.filter(id=number)
+    houses = House.objects.filter(id=id)
 
     # If no house found return a no house page
     # TODO make this it's own view and redirect instead
@@ -26,13 +26,33 @@ def house(request, number):
 
 def new_report(request, id):
     # Get relevant house
-    house = House.objects.filter(id=id)
+    houses = House.objects.filter(id=id)
 
     # Check house exists
-    if len(house) == 0:
+    if len(houses) == 0:
         return render(request, 'reviews/nohouse.html', {})
+    house = houses[0]
 
-    return render(request, 'reviews/house.html', {'house' : house[0]})
+    print(id)
+    print(house)
+
+    if(request.method == "POST"):
+        print("post")
+        print(request.POST)
+        form = HouseReportForm(request.POST)
+        print(form)
+        if(form.is_valid()):
+            print("valid")
+            report = form.save(commit=False)
+            report.house_filed = house
+            report.save()
+            print(report)
+            return redirect('house', id=house.id)
+        else:
+            print("Form Error")
+            print(form.errors)
+    form = HouseReportForm()
+    return render(request, 'reviews/newreport.html', {'new_report_form' : form, 'house' : house})
 
 
 def new_house(request):
@@ -47,9 +67,6 @@ def new_house(request):
             house.date_created = timezone.now()
             house.save()
             print(house.id)
-
-        return redirect('new_report', id=house.id)
-
-    else:
-        form = HouseForm()
-        return render(request, 'reviews/newhouse.html', {'new_house_form' : form})
+            return redirect('new_report', id=house.id)
+    form = HouseForm()
+    return render(request, 'reviews/newhouse.html', {'new_house_form' : form})
