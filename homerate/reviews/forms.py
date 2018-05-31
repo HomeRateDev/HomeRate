9d2a954ee2690e4e5c20d46b3e18702300e6c3ff
edward <es916@ic.ac.uk>
2018-05-31 17:48:41 +0100

    Create new form and html page for making new reports

--- homerate/reviews/forms.py
+++ homerate/reviews/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import House
+from .models import House, HouseReport
 
 
 class HouseForm(forms.ModelForm):
@@ -8,3 +8,15 @@ class HouseForm(forms.ModelForm):
     class Meta:
         model = House
         fields = ('addr',)
+
+
+class HouseReportForm(forms.ModelForm):
+
+    class Meta:
+        model = HouseReport
+        fields = ('author',
+                  'moved_in_date',
+                  'description',
+                  'pros',
+                  'cons',
+                  'monthly_cost',)
