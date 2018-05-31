32b51fc3be19d017d83b94dead333f65da633acb
edward <es916@ic.ac.uk>
2018-05-31 18:17:38 +0100

    Update url organisation

--- homerate/reviews/urls.py
+++ homerate/reviews/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'h(?P<id>\d+)$', views.house, name='house'),
+    url(r'(?P<id>\d+)$', views.house, name='house'),
     url(r'new_report/(?P<id>\d+)/$', views.new_report, name='new_report'),
     url(r'new_house$', views.new_house, name='new_house'),
 ]
