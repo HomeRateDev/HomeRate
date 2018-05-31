725fd37ae3ce7f763545295279cce52fc9b2b284
Zeshan Amjad <zeshan@zesh.me>
2018-05-31 19:40:11 +0100

    Improve URL schema for houses

--- homerate/reviews/urls.py
+++ homerate/reviews/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path
 
 urlpatterns = [
-    url(r'(?P<id>\d+)$', views.house, name='house'),
+    url(r'house/(?P<id>\d+)$', views.house, name='house'),
     url(r'new_report/(?P<id>\d+)/$', views.new_report, name='new_report'),
     url(r'new_house$', views.new_house, name='new_house'),
 ]
