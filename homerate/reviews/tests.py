e1d2b54f10d2da50cb28fb35d70e4a7e32081d1d
edward <es916@ic.ac.uk>
2018-06-01 11:37:49 +0100

    Add test case for saving and recalling the House model

--- homerate/reviews/tests.py
+++ homerate/reviews/tests.py
@@ -1,10 +1,20 @@
+from django.db.models.functions import datetime
 from django.test import TestCase
+from pytz import UTC
+
+from .models import House
 
 
 class ReviewsTest(TestCase):
-	def setUp(self):
-		return 0
+    def setUp(self):
+        return 0
 
-	def testCase1(self):
-		self.assertEqual(1,1)
-
+    def testMakeAndRecallHouseModel(self):
+        time1 = datetime.datetime(year=2018, month=5, day=30, tzinfo=UTC)
+        time2 = datetime.datetime(year=2018, month=2, day=15, tzinfo=UTC)
+        House.objects.create(address="10 Downing Street", date_created=time1)
+        House.objects.create(address="101 Dalmatian lane", date_created=time2)
+        house1 = House.objects.get(address="10 Downing Street")
+        house2 = House.objects.get(address="101 Dalmatian lane")
+        self.assertEqual(house1.date_created, time1)
+        self.assertEqual(house2.date_created, time2)
