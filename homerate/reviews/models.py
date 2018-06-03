87106265c3e841e1f5d8d93a5604b5fa124ddcc6
Zeshan Amjad <zeshan@zesh.me>
2018-06-03 12:04:50 +0100

    Display bedroom fields on House view

--- homerate/reviews/models.py
+++ homerate/reviews/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class House(models.Model):
     address = models.CharField(max_length=200)
     date_created = models.DateTimeField(default=timezone.now)
