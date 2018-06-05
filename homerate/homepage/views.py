fd3bbd9de296ef6712eea2a2310d315f21e877a6
edward <es916@ic.ac.uk>
2018-06-05 15:57:17 +0100

    Reorganised html templates structure and added new app for Profiles

--- homerate/homepage/views.py
+++ homerate/homepage/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from reviews.models import House
 
+
 # Create your views here.
 def homepage(request):
-	houses = House.objects.order_by('-date_created')
-	return render(request, 'homepage.html', {'houses': houses})
+    houses = House.objects.order_by('-date_created')
+    return render(request, 'homepage/homepage.html', {'houses': houses})
