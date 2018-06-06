c1bd36facf3a9a26f0f54a091415c2f2e892e7fc
Zeshan Amjad <zeshan@zesh.me>
2018-06-06 17:11:51 +0100

    Add SCSS support

--- homerate/homerate/settings.py
+++ homerate/homerate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sass_processor',
     'reviews',
     'homepage',
     'profiles' 
@@ -125,3 +126,9 @@ LOGIN_REDIRECT_URL = 'homepage'
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
