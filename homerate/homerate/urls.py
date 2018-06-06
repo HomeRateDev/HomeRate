e17540b13ccd5223335622550edc8c617c4f58aa
rep15 <rep15@ic.ac.uk>
2018-06-06 16:49:50 +0100

    Implement email activation

--- homerate/homerate/urls.py
+++ homerate/homerate/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include
+from django.conf.urls import include, url
 from django.urls import path
+from profiles import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- homerate/profiles/views.py
+++ homerate/profiles/views.py
@@ -1,25 +1,59 @@
+from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.auth.models import User
+from profiles.forms import SignupForm
+
+from profiles.tokens import account_activation_token
+
 import os
 
-# Create your views here.
 def sign_up(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = form.save(commit=False)
+            if user.email[-6:] != ".ac.uk":
+                return render(request, 'registration/signup_error.html')
+            user.username = user.email
             user.is_active = False
-            user.email = user.username
             user.save()
-            user.email_user("test","testing")
-            return redirect('homepage')
+            current_site = get_current_site(request)
+            subject = 'Activate your blog account.'
+            message = render_to_string('registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8'),
+                'token':account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+            return render(request, 'registration/please_activate.html')
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return render(request, 'registration/activation_successful.html')
+    else:
+        return render(request, 'registration/activation_unsuccessful.html')
+
+def account_activation_sent(request):
+    return render(request, 'registration/please_activate.html')
+
 
 def profile(request):
     print("profile")
