452339aa0cdff695bc4271b02adc2e42784f7738
Zeshan Amjad <zeshan@zesh.me>
2018-06-08 13:22:19 +0100

    Improve styling of sign in forms

--- homerate/profiles/forms.py
+++ homerate/profiles/forms.py
@@ -3,7 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
+    email = forms.EmailField(max_length=200, help_text='Required', label_suffix='')
+    first_name = forms.EmailField(max_length=20, label_suffix='')
+    password1 = forms.CharField(widget=forms.PasswordInput(), label_suffix='', label='Password')
+    password2 = forms.CharField(widget=forms.PasswordInput(), label_suffix='', label='Password Confirmation')
+
     class Meta:
         model = User
         fields = ('email', 'first_name', 'password1', 'password2')
