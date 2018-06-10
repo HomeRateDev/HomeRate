b2b6b5498a194ec0247f6689d80b9d502e4b29b5
Zeshan Amjad <zeshan@zesh.me>
2018-06-10 20:58:01 +0100

    Correct typo in postcode regex

--- homerate/address_api/urls.py
+++ homerate/address_api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(
         # Regex to match UK postcodes
-        r'postcode/(?P<postcode>^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2}))/$',
+        r'postcode/(?P<postcode>(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2}))/$',
         views.api_request,
         name='address_api')
 ]
