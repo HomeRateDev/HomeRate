23a69b4d9e60c9050b7e2d44fc0ac7245ffeb2f5
Zeshan Amjad <zeshan@zesh.me>
2018-06-10 22:37:54 +0100

    Implement front-end homepage integration with address API

--- homerate/homepage/static/js/homepage.js
+++ homerate/homepage/static/js/homepage.js
@@ -1,38 +1,62 @@
-(function($) {
-    $("li").mousedown(function() {
-		window.location.href = $(this).children("a").attr("href");
-	});
-	$(".searchBox").keyup(function() {
-        const query = $(this).val();
-        const postCode = new RegExp('(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})', 'i');
+(function ($) {
+    function populateAddresses(query) {
+        /* Strip trailing whitespace and remove (optional) space within postcode */
+        const postcode = query.trim().replace(' ', '');
+        $.get({
+            'url': '/address_api/postcode/' + postcode,
+            success: function (data) {
+                /* Delete everything in the autocomplete box */
+                $('.autocomplete').empty();
 
+                const addresses = data.addresses;
+
+                /* Generate list entries with URLs for each address
+                 * and appends them to the autocomplete box */
+                addresses.forEach(function (address) {
+                    const formattedAddr = formatAddress(address, postcode),
+                          entry = $('<li/>').addClass('entry'),
+                          url = '/reviews/check_address/' + encodeURIComponent(address),
+                          link = $('<a/>').attr('href', url).html(formattedAddr);
+                    link.appendTo(entry);
+                    entry.appendTo('.autocomplete');
+                });
+            }
+        })
+    }
+
+    /* Takes a raw address string (e.g. "1 Stratford Avenue, , , , , Rochdale, Lancashire")
+     * and a postcode. Strips empty components of the address and returns a string containing
+     * each non-empty component (including the postcode) separated by ", " */
+    function formatAddress(rawAddress, postcode) {
+
+        /* Split the string into an array of components using the comma as a delimiter */
+        let components = rawAddress.split(","),
+            result = "";
+
+        for (let i = 0; i < components.length; i++) {
+            /* If component is non-empty */
+            if (components[i].trim().length > 0) {
+                result += components[i] + ", ";
+
+                /* Append the postcode if we're at the last component */
+                if (i === components.length - 1) {
+                    result += postcode
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /* After a key is typed in the search box */
+    $('.searchBox').keyup(function () {
+        const query = $(this).val(),
+              postCode = new RegExp('(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})', 'i');
+
+        // If current input is a valid postcode, populate the autocomplete box
         if (postCode.test(query)) {
-
+            populateAddresses(query);
         }
+    })
 
-        $(".autocomplete").css('height', 'auto');
-		if (query === "") {
-			$(".autocomplete").css({
-                'height': 'auto',
-                'visibility': 'visible'
-            });
-		} else {
-			$("ul li:contains('" + query + "')").show();
-			$("ul li").not(":contains('" + query + "')").hide();
-		}
-	}).focusout(function() {
-        console.log(document.activeElement);
-	    if (!$(document.activeElement).hasClass('.entry')) {
-	        $(".autocomplete").css({
-                'height': '0',
-                'visibility': 'hidden'
-            });
-        }
-
-    }).focus(function() {
-        $(".autocomplete").css({
-            'height': 'auto',
-            'visibility': 'visible'
-	    });
-    });
 })(jQuery);
