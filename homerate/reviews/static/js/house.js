40b136b807e70ffb2a00ee9bc58361622ad6272f
Zeshan Amjad <zeshan@zesh.me>
2018-06-12 14:39:49 +0100

    Implement commute times information module using Distance Matrix API

--- homerate/reviews/static/js/house.js
+++ homerate/reviews/static/js/house.js
@@ -1,9 +1,29 @@
-(function($) {
+function getCommuteTimes() {
+    const origin = $('.houseAddress').data('postcode'),
+          destination = 'SW7 2AZ',
+          modes = ['walking', 'bicycling', 'transit', 'driving'];
+
+    modes.forEach(function (mode) {
+        const service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix({
+                origins: [origin],
+                destinations: [destination],
+                travelMode: mode.toUpperCase()
+        }, (response, status) => insertCommuteTimes(mode, response));
+    });
+}
+
+function insertCommuteTimes(mode, response) {
+    const time = response.rows[0].elements[0].duration.text;
+    $('.route.' + mode + ' .timeValue').html(time);
+}
+
+(function ($) {
 
     /* Converts text rating values into visual stars using BarRating.js */
     function createVisualStars() {
         const starFields = $('.createStars');
-        starFields.each(function(i, field) {
+        starFields.each(function (i, field) {
             /* Wrap the star field in a jQuery object */
             field = $(field);
 
@@ -11,7 +31,7 @@
             *  and generate HTML for the select field needed to
             *  display visual stars */
             const value = parseInt(field.html()),
-                  select = $('<select/>').addClass('starRating');
+                select = $('<select/>').addClass('starRating');
 
             /* If the value couldn't be parsed, the field wasn't filled
              * out in the review, so don't display this review point. */
