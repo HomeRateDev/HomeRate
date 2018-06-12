a9d78bc8f26d93ca7b934e62ccce7f2fca3cc3c2
Zeshan Amjad <zeshan@zesh.me>
2018-06-12 16:55:58 +0100

    Implement nearest shop module using Places API

--- homerate/reviews/static/js/house.js
+++ homerate/reviews/static/js/house.js
@@ -1,18 +1,71 @@
+function initMapping() {
+    getCommuteTimes();
+    getNearestShops();
+}
+
+/* Returns the postcode for the current house */
+function getPostcode() {
+    return $('.houseAddress').data('postcode');
+}
+
+/* Retrieves the time taken to commute to the destination
+ * via 4 different methods and inserts it into the page. */
 function getCommuteTimes() {
-    const origin = $('.houseAddress').data('postcode'),
-          destination = 'SW7 2AZ',
-          modes = ['walking', 'bicycling', 'transit', 'driving'];
+    const origin = getPostcode(),
+        destination = 'SW7 2AZ',
+        modes = ['walking', 'bicycling', 'transit', 'driving'];
 
     modes.forEach(function (mode) {
         const service = new google.maps.DistanceMatrixService();
         service.getDistanceMatrix({
-                origins: [origin],
-                destinations: [destination],
-                travelMode: mode.toUpperCase()
+            origins: [origin],
+            destinations: [destination],
+            travelMode: mode.toUpperCase()
         }, (response, status) => insertCommuteTimes(mode, response));
     });
 }
 
+function getNearestShops() {
+    geocodeAddress(getPostcode(), insertNearestShops);
+}
+
+/* Takes an address, converts it to LatLng and passes it to callback */
+function geocodeAddress(address, callback) {
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status === 'OK') {
+            const location = results[0].geometry.location,
+                  coords = {lat: location.lat(), lng: location.lng()};
+            callback(coords);
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+function insertNearestShops(location) {
+    const service = new google.maps.places.PlacesService(document.querySelector('.mapsHook'));
+    service.nearbySearch({
+        location: new google.maps.LatLng(location),
+        radius: 1000,
+        type: ['store']
+    }, function(response) {
+        window.rsp = response;
+        for (let i = 0; i < response.length; i++) {
+            let place = response[i];
+            if (place.types.includes('grocery_or_supermarket')) {
+                const shop = $("<li/>").addClass('shop'),
+                      icon = $("<i class='far fa-store'></i>"),
+                      shopName = $("<span/>").addClass('shopName').html(place.name);
+                shop.appendTo('.shops');
+                icon.appendTo(shop);
+                shopName.appendTo(shop);
+            }
+        }
+    });
+}
+
+/* Parses commute time response and injects it into the DOM */
 function insertCommuteTimes(mode, response) {
     const time = response.rows[0].elements[0].duration.text;
     $('.route.' + mode + ' .timeValue').html(time);
