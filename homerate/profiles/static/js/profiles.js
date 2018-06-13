4700f77b4b4501d1d21ac68c5a28ea800efe8280
Zeshan Amjad <zeshan@zesh.me>
2018-06-13 15:44:19 +0100

    Merge postcode validation

--- homerate/profiles/static/js/profiles.js
+++ homerate/profiles/static/js/profiles.js
@@ -22,34 +22,4 @@
         validatePostcode()
     });
 
-    /* Signup Form Password Validation */
-    $('#id_password1').keyup(function () {
-        const passwordInput = $(this),
-            password = passwordInput.val();
-
-        const lengthReq = $('.requirement.length'),
-            numericReq = $('.requirement.numeric');
-
-        /* Check if the current password is entirely numeric */
-        let entirelyNumeric = true;
-        for (let i = 0; i < password.length; i++) {
-            entirelyNumeric &= isDigit(password[i]);
-        }
-
-        requirementChange(lengthReq, password.length >= 8);
-        requirementChange(numericReq, !entirelyNumeric);
-
-    });
-
-    /* Marks requirements as valid/invalid and hides/displays them, based on the
-     * 'valid' flag */
-    function requirementChange(req, valid) {
-        if (valid) {
-            req.removeClass('invalid').addClass('valid').slideUp(300);
-            req.find('i').removeClass('fa-times-circle').addClass('fa-check-circle');
-        } else {
-            req.addClass('invalid').removeClass('valid').slideDown(300);
-            req.find('i').addClass('fa-times-circle').removeClass('fa-check-circle');
-        }
-    }
 })(jQuery);
