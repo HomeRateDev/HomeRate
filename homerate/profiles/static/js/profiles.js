b51383aad18647f00e70dd8b120e0506263e6cc9
Rohan Padmanabhan <rohan@padmanabhan.net>
2018-06-13 10:48:51 +0100

    Move inline javascript into javscript file.

--- homerate/profiles/static/js/profiles.js
+++ homerate/profiles/static/js/profiles.js
@@ -1,19 +1,44 @@
 (function ($) {
 
-    /* After a key is typed in the search box */
-    $('.postcode').keyup(function (event) {
+    $(function() {
 
-        const inputCode = $('#id_postcode').val()
-              postCodeRegex = new RegExp('^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$', 'i');
+        /* Set the optional bar ratings */
+        $('.weighting').barrating({
+            theme: 'bars-square',
+            allowEmpty: true,
+            initialRating: 0,
+            deselectable: true,
+            showValues: true,
+            showSelectedRating: false
+        });
 
-        /* Test if the current value matches the regex */
-        if (postCodeRegex.test(inputCode)) {
-            /* If current input is a valid postcode, enable the submission box and make full opacity. */
-            $("#postcodeSubmit").removeAttr('disabled').css({"opacity": 1});
-        } else {
-            /* If the current input is invalid, disbale the submission box and reduce opacity. */
-            $("#postcodeSubmit").attr('disabled', 'disabled').css({"opacity": 0.7});
-        }
-    })
+
+        /* Set the mandatory bar ratings */
+        $('.weightingMandatory').barrating({
+            theme: 'bars-square',
+            showValues: true,
+            showSelectedRating: false
+        });
+
+
+        /* After a key is typed in the search box */
+        $('.postcode').keyup(function (event) {
+
+            /* Value currently in the postcode entry box */
+            const inputCode = $('#id_postcode').val()
+
+            /* Test regex expression for postcode*/
+            postCodeRegex = new RegExp('^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$', 'i');
+
+            /* Test if the current value matches the regex */
+            if (postCodeRegex.test(inputCode)) {
+                /* If current input is a valid postcode, enable the submission box and make full opacity. */
+                $("#postcodeSubmit").removeAttr('disabled').css({"opacity": 1});
+            } else {
+                /* If the current input is invalid, disbale the submission box and reduce opacity. */
+                $("#postcodeSubmit").attr('disabled', 'disabled').css({"opacity": 0.7});
+            }
+        })
+    });
 
 })(jQuery);
