37426100cb14ef3effc3c5fba29d5b6e5ecdd0cc
Rohan Padmanabhan <rohan@padmanabhan.net>
2018-06-13 11:46:42 +0100

    Extract postcode validation to separate file for reuse.

---
+++ homerate/profiles/static/js/profilePostcode.js
@@ -0,0 +1,14 @@
+/* Validate the postcode and set the save box accordingly. */
+function validatePostcode() {
+    const inputCode = $('#id_postcode').val()
+    postCodeRegex = new RegExp('(^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$)|^$', 'i');
+
+    /* Test if the current value matches the regex */
+    if (postCodeRegex.test(inputCode)) {
+        /* If current input is a valid postcode, enable the submission box and make full opacity. */
+        $("#postcodeSubmit").removeAttr('disabled').css({"opacity": 1});
+    } else {
+        /* If the current input is invalid, disbale the submission box and reduce opacity. */
+        $("#postcodeSubmit").attr('disabled', 'disabled').css({"opacity": 0.7});
+    }
+};
