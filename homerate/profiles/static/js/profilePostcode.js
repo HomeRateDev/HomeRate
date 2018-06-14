52b9009fe62d53c65016e51fcec3500ef12266e0
Zeshan Amjad <zeshan@zesh.me>
2018-06-14 23:30:56 +0100

    Correct bug where postcode formatter was always adding a space to postcode fields

--- homerate/profiles/static/js/profilePostcode.js
+++ homerate/profiles/static/js/profilePostcode.js
@@ -1,19 +1,24 @@
 /* Validate the postcode and set the save box accordingly. */
 function validatePostcode() {
-    const inputCode = $('#id_postcode').val().replace(/ /g,''),
-    postCodeRegex = new RegExp('(^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$)|^$', 'i');
+    const postcodeField = $('#id_postcode'),
+        postcodeSubmit = $("#postcodeSubmit"),
+        inputCode = postcodeField.val().replace(/ /g, ''),
+        postCodeRegex = new RegExp('(^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$)|^$', 'i');
 
     /* Test if the current value matches the regex */
     if (postCodeRegex.test(inputCode)) {
         /* If current input is a valid postcode, enable the submission box and make full opacity. */
-        $("#postcodeSubmit").removeAttr('disabled').css({"opacity": 1});
-        $('#id_postcode').val(spacePostcode($('#id_postcode').val().toUpperCase().replace(/ /g,'')))
+        postcodeSubmit.removeAttr('disabled').css({"opacity": 1});
+        postcodeField.val(spacePostcode(postcodeField.val().toUpperCase().replace(/ /g, '')))
     } else {
         /* If the current input is invalid, disbale the submission box and reduce opacity. */
-        $("#postcodeSubmit").attr('disabled', 'disabled').css({"opacity": 0.7});
+        postcodeSubmit.attr('disabled', 'disabled').css({"opacity": 0.7});
     }
 };
 
 function spacePostcode(postcode) {
-  return postcode.slice(0, -3) + " " + postcode.slice(-3)
+    if (postcode === "") {
+        return "";
+    }
+    return postcode.slice(0, -3) + " " + postcode.slice(-3)
 };
