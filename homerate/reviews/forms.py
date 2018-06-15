898da40a3c8eb367b10a79b103845101e0aa504a
Zeshan Amjad <zeshan@zesh.me>
2018-06-15 12:45:44 +0100

    Add placeholder text to comment textarea fields on review page

--- homerate/reviews/forms.py
+++ homerate/reviews/forms.py
@@ -40,9 +40,25 @@ class HouseReportForm(forms.ModelForm):
                 'data-toggle': 'datepicker'
             }
         )
-        self.fields['landlord_comment'].widget = forms.Textarea()
-        self.fields['construction_quality_comment'].widget = forms.Textarea()
-        self.fields['nuisances_comment'].widget = forms.Textarea()
-        self.fields['affordability_comment'].widget = forms.Textarea()
+        self.fields['landlord_comment'].widget = forms.Textarea(
+            attrs={
+                'placeholder': 'Add your comment about the landlord...'
+            }
+        )
+        self.fields['construction_quality_comment'].widget = forms.Textarea(
+            attrs={
+                'placeholder': "Add your comment about the house's construction quality..."
+            }
+        )
+        self.fields['nuisances_comment'].widget = forms.Textarea(
+            attrs={
+                'placeholder': 'Add your comment about any nuisances in the property...'
+            }
+        )
+        self.fields['affordability_comment'].widget = forms.Textarea(
+            attrs={
+                'placeholder': "Add your comment about the house's affordability..."
+            }
+        )
 
 
